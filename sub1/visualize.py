bec7eb2d0b4e98997a8f7fccfa94959b3b0b1274
hajunge <haha950903@naver.com>
2021-09-01 15:56:15 +0900

    req1,2,3 완료
    Former-commit-id: 4f949806d67de47a55ff5b87d0922e7fecd30793

--- sub1/visualize.py
+++ sub1/visualize.py
@@ -5,6 +5,9 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
+import folium
+from folium.plugins import MarkerCluster
+
 
 def set_config():
     # 폰트, 그래프 색상 설정
@@ -31,24 +34,13 @@ def show_store_categories_graph(dataframes, n=100):
     stores = dataframes["stores"]
 
     # 모든 카테고리를 1차원 리스트에 저장합니다
-    print(stores,'1')
-
     categories = stores.category.apply(lambda c: c.split("|"))
-    print(categories,'2')
-
     categories = itertools.chain.from_iterable(categories)
-    print(categories,'3')
 
     # 카테고리가 없는 경우 / 상위 카테고리를 추출합니다
     categories = filter(lambda c: c != "", categories)
-    print(categories,'4')
-
     categories_count = Counter(list(categories))
-    print(categories,'5')
-
     best_categories = categories_count.most_common(n=n)
-    print(best_categories,'6')
-
     df = pd.DataFrame(best_categories, columns=["category", "count"]).sort_values(
         by=["count"], ascending=False
     )
@@ -63,13 +55,11 @@ def show_store_categories_graph(dataframes, n=100):
 def show_store_review_distribution_graph(dataframes):
     """
     Req. 1-3-1 전체 음식점의 리뷰 개수 분포를 그래프로 나타냅니다. 
-    리뷰가 0 개인게 몇개
-    1개인게 몇개
-    2개인게 몇개 이거인듯?
     """
+
     data = dataframes["stores"]
     df = data["review_cnt"].value_counts().reset_index()
-    # 그래프 그리기
+
     chart = sns.barplot(x="index", y="review_cnt", data=df)
     chart.set_xticklabels(chart.get_xticklabels(), rotation=45)
     plt.title("음식점 리뷰 개수 분포")
@@ -78,10 +68,23 @@ def show_store_review_distribution_graph(dataframes):
     raise NotImplementedError
 
 
-def show_store_average_ratings_graph():
+def show_store_average_ratings_graph(dataframes):
     """
     Req. 1-3-2 각 음식점의 평균 평점을 그래프로 나타냅니다.
     """
+
+    data = pd.merge(
+        dataframes["stores"], dataframes["reviews"], left_on="id", right_on="store"
+    )
+    data = data.groupby(["store", "store_name"])
+    data = data.mean()
+    df = data["score"].value_counts().reset_index().round(1)
+
+    chart = sns.barplot(x="index", y="score", data=df)
+    chart.set_xticklabels(chart.get_xticklabels(), rotation=45)
+    plt.title("음식점 평균 평점")
+    plt.show()
+
     raise NotImplementedError
 
 
@@ -89,6 +92,14 @@ def show_user_review_distribution_graph(dataframes):
     """
     Req. 1-3-3 전체 유저의 리뷰 개수 분포를 그래프로 나타냅니다.
     """
+
+    df = dataframes["reviews"]["user"].value_counts().head(n=20).reset_index()
+
+    chart = sns.barplot(x="index", y="user", data=df)
+    chart.set_xticklabels(chart.get_xticklabels(), rotation=45)
+    plt.title("유저 리뷰 개수 분포")
+    plt.show()
+
     raise NotImplementedError
 
 
@@ -96,6 +107,22 @@ def show_user_age_gender_distribution_graph(dataframes):
     """
     Req. 1-3-4 전체 유저의 성별/나이대 분포를 그래프로 나타냅니다.
     """
+
+    data = dataframes["users"]
+    df = data["gender"].value_counts().reset_index()
+
+    chart = sns.barplot(x="index", y="gender", data=df)
+    chart.set_xticklabels(chart.get_xticklabels(), rotation=45)
+    plt.title("유저 성별 분포")
+    plt.show()
+
+    df = data["age"].value_counts().reset_index()
+
+    chart = sns.barplot(x="index", y="age", data=df)
+    chart.set_xticklabels(chart.get_xticklabels(), rotation=45)
+    plt.title("유저 나이대 분포")
+    plt.show()
+
     raise NotImplementedError
 
 
@@ -103,6 +130,16 @@ def show_stores_distribution_graph(dataframes):
     """
     Req. 1-3-5 각 음식점의 위치 분포를 지도에 나타냅니다.
     """
+
+    data = dataframes["stores"].head(n=100).reset_index()
+
+    map = folium.Map([37.556862,126.926666], zoom_start=9)
+    marker_cluster = MarkerCluster().add_to(map)
+
+    for i in range(len(data)):
+        folium.Marker([data["latitude"][i], data["longitude"][i]], tooltip=data["store_name"][i]).add_to(marker_cluster)
+    map.save("map.html")
+
     raise NotImplementedError
 
 
@@ -110,7 +147,11 @@ def main():
     set_config()
     data = load_dataframes()
     # show_store_categories_graph(data)
-    show_store_review_distribution_graph(data)
+    # show_store_review_distribution_graph(data)
+    # show_store_average_ratings_graph(data)
+    # show_user_review_distribution_graph(data)
+    # show_user_age_gender_distribution_graph(data)
+    show_stores_distribution_graph(data)
 
 if __name__ == "__main__":
     main()
