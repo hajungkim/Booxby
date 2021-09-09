41a8e29edcb168d38e70f90c225e9bcbf951f1a0
박선주 <cucu9823@naver.com>
2021-09-09 19:57:19 +0900

    [FE] ADD : SignUp form
    Former-commit-id: d713143fbab27c4a376922d225e416da571251cf

--- frontend/src/pages/Signup.vue
+++ frontend/src/pages/Signup.vue
@@ -1,15 +1,196 @@
 <template>
-  <div>
-    dsadasd
+  <div class="signup_container">
+    <div class="signup_wrap">
+      <div class="text-group">
+        <h3 class="m-0">회원가입</h3>
+      </div>
+      <div class="form-group">
+        <div class="form-input-email">
+          <q-input
+              class="input"
+              label="Email"
+              type="email"
+              v-model="form.email"
+              lazy-rules
+              :rules="[
+              val => !!val || '필수입력항목 입니다.',
+              checkEmail
+              ]"
+            />
+          <div class="mail_button">
+            <button><span>인증 메일 발송</span></button>
+          </div>
+        </div>
+
+        <div class="form-mb">
+          <q-input class="p-0" label="인증 번호" />
+          <div>
+            <!-- 아이콘 -->
+            <span>인증 완료 or 인증번호 틀림</span>
+          </div>
+        </div>
+
+        <div class="form-mb">
+          <q-input class="input" label="Password" color="teal" v-model="form.password" type="password"
+            lazy-rules
+              :rules="[
+                val => val && val.length > 8 || '8자리 이상 입력해주세요.',
+                checkPassWord
+              ]"/>
+        </div>
+
+        <div class="form-mb">
+          <q-input label="비밀번호 재입력" type="password" v-model="form.passwordconfirmation"
+            lazy-rules
+              :rules="[
+                val => val && val.length > 0 || '필수입력항목 입니다.',
+                checkPassWordConfirmation
+              ]"/>
+        </div>
+
+        <div>
+          <q-input label="닉네임"
+            v-model="form.nickname"
+            lazy-rules
+              :rules="[
+                val => val && val.length > 2 || '두글자 이상 입력 바랍니다.',
+                checkName
+              ]"/>
+        </div>
+        <!-- 성별 -->
+        <div style="margin-top:10px;">
+          <q-radio style="margin-right:10px; margin-left:-10px;" keep-color v-model="color" val="cyan" label="남자" color="cyan" />
+          <q-radio keep-color v-model="color" val="red" label="여자" color="red" />
+        </div>
+        <!-- 나이 -->
+        <div>
+           <q-select v-model="model" :options="options" label="연령대" />
+        </div>
+        <!-- 버튼 -->
+        <div class="submit_bt">
+          <q-btn class="submit" color="primary" label="회원가입하기" />
+        </div>
+
+      </div>
+    </div>
+    <div class="signupImg">
+      <!-- <img src="" alt=""> -->
+      Booxby
+    </div>
   </div>
 </template>
 
 <script>
+import { reactive } from 'vue'
+import { ref } from 'vue'
+// import axios from 'axios'
 export default {
+  setup(){
+    const form = reactive({
+      email: '',
+      password: '',
+      passwordconfirmation: '',
+      nickname: '',
+    })
+    // const options = [
+    //     '유아', '초등학생', '청소년', '10대', '20대', '30대', '40대' ,'50대', '60대 이상'
+    //   ]
+    function checkName (val) {
+      const reg = /[ㄱ-ㅎㅏ-ㅣ가-힣]/g
+      return (reg.test(val)||'한글만 입력가능합니다.')
+    }
+    // 비밀번호 유효성 체크
+    function checkPassWord (val) {
+      const reg = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
+      return (reg.test(val)||'최소 각 하나의 문자, 숫자, 특수 문자가 포함된 8자리 이상이어야 합니다.')
+    }
+    function checkPassWordConfirmation (val) {
+      if (form.password != val){
+        return ('비밀번호가 일치하지 않습니다.')
+      }
+    }
+    // 이메일 유효성 체크
+    function checkEmail (val) {
+      const reg = /^[0-9a-zA-Z]([-.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i
+      return (reg.test(val)||'이메일 형식이 잘못되었습니다.')
+    }
 
+    // function sendEmail() {
+		// 	const data = {
+		// 			'email': this.credentials.email
+		// 		}
+    //   HTTP.post('/email/auth/', data)
+    //   .then(() => {
+    //     this.certification.isSend = true
+    //   })
+    //   .catch(err => {
+    //     console.error(err)
+    //   })
+    // }
+
+    return {
+      model: ref(null),
+      options: [
+        '유아', '초등학생', '청소년', '10대', '20대', '30대', '40대' ,'50대', '60대 이상'
+      ],
+      color: ref('cyan'),
+      form,
+      checkName,
+      checkPassWord,
+      checkEmail,
+      checkPassWordConfirmation
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+@import "../css/app.scss";
+.signup_container{
+  display:flex;
+}
+.signup_wrap{
+  display:flex;
+  flex-direction: column;
+  width:800px;
+  height:798px;
+  justify-content: center;
+  align-items: center;
+  margin-top:40px;
+}
+.text-group{
+  display: flex;
+  justify-content: center;
+}
+.form-group{
+  width:500px;
+  height:700px;
+  margin-top:20px;
+}
+.form-mb{
+  margin-top:8px;
+  margin-bottom:8px;
+}
+.mail_button{
+  float:right;
+  margin: -15px 0px;
+}
+.signupImg{
+  float:right;
+  width:800px;
+  height:798px;
+  background-color : lightblue;
+  border-top-right-radius: 38px;
+  border-bottom-right-radius: 38px;
+}
+.submit_bt{
+  display: flex;
+  justify-content: center;
+  margin-top:40px;
+}
+.submit{
+  width:300px;
+  height:60px;
+  border-radius: 15px;
+}
 </style>
