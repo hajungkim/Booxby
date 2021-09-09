a05cb630713db6434ac23b9d7dcd097e1995c054
박선주 <cucu9823@naver.com>
2021-09-09 15:38:17 +0900

    [FE] ADD : mainBox추가
    Former-commit-id: 32be409b1dc3f4376af3b74275f2038a1a6be262

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -2,9 +2,9 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    component: () => import('layouts/CommonBox.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: '', component: () => import('pages/Signup.vue') }
     ]
   },
 
