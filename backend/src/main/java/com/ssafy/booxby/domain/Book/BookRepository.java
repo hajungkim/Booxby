f7ec5e3cd98c1ff037ca110544a6947d73005eda
lee.sujeong10111 <lee.sujeong10111@gmail.com>
2021-09-13 11:37:55 +0900

    [ADD] 책 상세/작가의 다른 책 조회 기능 추가
    Former-commit-id: 4abb96b25aeaa32f21ea5eaecb8aa35662712210

--- backend/src/main/java/com/ssafy/booxby/domain/Book/BookRepository.java
+++ backend/src/main/java/com/ssafy/booxby/domain/Book/BookRepository.java
@@ -2,7 +2,11 @@ package com.ssafy.booxby.domain.Book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     Book findByBookId(Long bookId);
+
+    List<Book> findByAuthor(String author);
 }
--- backend/src/main/java/com/ssafy/booxby/web/BookController.java
+++ backend/src/main/java/com/ssafy/booxby/web/BookController.java
@@ -2,8 +2,12 @@ package com.ssafy.booxby.web;
 
 import com.ssafy.booxby.domain.Book.Book;
 import com.ssafy.booxby.service.BookService;
+import com.ssafy.booxby.web.dto.BookDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @CrossOrigin(origins = { "*" }, maxAge = 6000)
 @RequiredArgsConstructor
@@ -18,9 +22,27 @@ public class BookController {
         ControllerResponse response = null;
 
         Book book = bookService.findBookById(bookId);
-        response = new ControllerResponse("success", book);
+        BookDto.DetailResponseDto detailResponseDto = new BookDto.DetailResponseDto(book);
 
-        System.out.println(book.getTitle());
+        response = new ControllerResponse("success", detailResponseDto);
+
         return response;
     }
+
+    @GetMapping("/author/{author}")
+    public ControllerResponse getAuthorBook(@PathVariable String author){
+        ControllerResponse response = null;
+
+        List<Book> bookList = bookService.findBookByAuthor(author);
+        List<BookDto.AuthorResponseDto> responseList = new ArrayList<>();
+
+        for(Book book:bookList){
+            BookDto.AuthorResponseDto authorResponseDto = new BookDto.AuthorResponseDto(book);
+            responseList.add(authorResponseDto);
+        }
+
+        response = new ControllerResponse("success", responseList);
+        return response;
+    }
+
 }
