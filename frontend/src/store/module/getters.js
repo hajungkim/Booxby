f90544ccc8e129da95096cb5d97eca0ffce9c21c
박선주 <cucu9823@naver.com>
2021-09-13 17:56:14 +0900

    [FE] UPDATE : myPage 찜목록 책 card로 변경
    Former-commit-id: 58d1da17a40a6e7f7f3f530c1f065903fe88ad2f

--- frontend/src/store/module/getters.js
+++ frontend/src/store/module/getters.js
@@ -1,2 +1,3 @@
-export function someGetter (/* state */) {
+export function getTextMode (state) {
+  return state.textMode
 }
--- frontend/src/store/module/state.js
+++ frontend/src/store/module/state.js
@@ -1,5 +1,6 @@
 export default function () {
+  const textMode = false
   return {
-    //
+    textMode,
   }
 }
