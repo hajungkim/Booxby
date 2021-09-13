534526a7727ad63c2cd7ee8313b39ee33114544a
박선주 <cucu9823@naver.com>
2021-09-13 14:32:37 +0900

    [FE] UDPATE : MyPage 정적페이지 마무리
    Former-commit-id: 4c21b23d594bd486b4380e12d61ce77b5bb69b02

--- frontend/src/layouts/CommonBox.vue
+++ frontend/src/layouts/CommonBox.vue
@@ -18,11 +18,13 @@ export default {
   height:650px;
   border: 1px solid black;
   border-radius: 38px;
+  background-color: rgb(227,231,234);
 }
 .container{
   display:flex;
   justify-content: center;
   align-items: center;
   height: 100vh;
+  background-color: rgb(185, 217, 241);
 }
 </style>
