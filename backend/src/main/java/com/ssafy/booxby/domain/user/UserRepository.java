7613c7f618b0c81f2c80535caa3b6fdaeb537833
hajungkim <haha950903@naver.com>
2021-09-16 11:00:31 +0900

    [BE] UPDATE : 회원가입에 월드컵점수,해시점수 추가
    Former-commit-id: 81b43af2d0c5e8bec15f1ad809865af57458f306

--- backend/src/main/java/com/ssafy/booxby/domain/user/UserRepository.java
+++ backend/src/main/java/com/ssafy/booxby/domain/user/UserRepository.java
@@ -2,8 +2,6 @@ package com.ssafy.booxby.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
