ffc7c76974866c945b5ade30458db30b50c80270
박선주 <cucu9823@naver.com>
2021-09-27 11:28:49 +0900

    [FE] UDPATE : signUp 기능구현완료
    Former-commit-id: 8b7e82f0b510bfd04baf855380760397fd1e74fc

--- frontend/src/pages/Worldcup.vue
+++ frontend/src/pages/Worldcup.vue
@@ -2,11 +2,11 @@
     <div class="worldcup_container">
         <div class="worldcup">
             <div class="worldcup_main">
-                <Oxbook :abc="select_books" :change="change"></Oxbook>
-                <q-card-actions style="margin: 10px 0px 0px 70px;">
-                    <q-icon @click="signUp" class="btn" style="font-size: 4.0em; color: red; margin-left:50px;" name="close"/>
-                    <q-icon class="btn" style="font-size: 4.0em; color: orange;" name="change_history"/>
-                    <q-icon @click="next" class="btn" style="font-size: 4.0em; color: green;" name="done"/>
+                <Oxbook :abc="select_books"></Oxbook>
+                <q-card-actions style="margin: 10px 0px 0px 75px;">
+                    <q-icon @click="Bad" class="btn" style="font-size: 4.0em; color: red; margin-left:50px;" name="close"/>
+                    <q-icon @click="next" class="btn" style="font-size: 4.0em; color: orange;" name="change_history"/>
+                    <q-icon @click="Like" class="btn" style="font-size: 4.0em; color: green;" name="done"/>
                 </q-card-actions>
             </div>
         </div>
@@ -15,6 +15,7 @@
 
 <script>
 import Oxbook from '../components/Oxbook.vue'
+import { useRouter } from 'vue-router'
 import { useStore } from 'vuex'
 // import { onMounted } from 'vue'
 export default {
@@ -23,21 +24,40 @@ export default {
     },
     setup() {
         const store = useStore()
+        const router = useRouter()
+        const Swal = require('sweetalert2')
+
         const infos = store.getters['module/getInfos']
         const hashtags_list = (store.getters['module/getHashtags'])
         const oxbooks = store.getters['module/getOxbooks']
 
-        let change = true
+        let final_score = 0
+
+
         let select_books = {}
         let i = 0
         select_books = oxbooks[i]
         store.commit('module/setselectOxbooks', oxbooks[i])
+
         function next(){
-        i = i + 1
-        select_books = oxbooks[i]
-        change = !change
-        store.commit('module/setselectOxbooks', oxbooks[i])
-        console.log(i,select_books,'selectoxbook')
+            i = i + 1
+            if (i==7){
+                final_score = final_score / 7
+                final_score = Math.round(final_score)
+                signUp()
+                return
+            }
+            select_books = oxbooks[i]
+            store.commit('module/setselectOxbooks', oxbooks[i])
+        }
+
+        function Bad(){
+            final_score = final_score - parseInt(select_books.emotion_score)
+            next()
+        }
+        function Like(){
+            final_score = final_score + parseInt(select_books.emotion_score)
+            next()
         }
         function signUp(){
             let hashtag = ''
@@ -47,21 +67,41 @@ export default {
             store.dispatch('module/signup',{
                 age: infos.age,
                 email: infos.email,
-                emotionScore:infos.emotionScore,
+                hashScore:infos.hashScore,
+                worldcupScore:final_score,
                 gender: infos.gender,
                 hashtag: hashtag,
                 nickname: infos.nickname,
                 password: infos.password,
             }).then((res)=>{
-                console.log(res,'res')
+                console.log(res.data.status,'res')
+                if (res.data.status == 'success'){
+                    Swal.fire({
+                        icon: 'success',
+                        title: '<span style="font-size:25px;">회원 가입이 완료 되었습니다.</span>',
+                        confirmButtonColor: '#ce1919',
+                        confirmButtonText: '<span style="font-size:18px;">확인</span>'
+                    })
+                }
+                else{
+                    Swal.fire({
+                        icon: 'error',
+                        title: '<span style="font-size:25px;">오류발생 회원가입을 다시 시도해 주세요.</span>',
+                        confirmButtonColor: '#ce1919',
+                        confirmButtonText: '<span style="font-size:18px;">확인</span>'
+                    })
+                }
+                router.push('/')
             }).catch((err)=>{
                 console.log(err,'err')
             })
+            
         }
         return{
             select_books,
             i,
-            change,
+            Bad,
+            Like,
             signUp,
             next
         }
